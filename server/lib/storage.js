c45df51a28217ba29f5d7acf787455da57edfd82
Valkomen <danielpena@MacBook-Air-de-Valkomen.local>
2026-03-06 10:06:20 -0500

    Fix: Bypass Vercel reserved AWS env vars v1.3.1

--- server/lib/storage.js
+++ server/lib/storage.js
@@ -8,8 +8,8 @@ console.log(`Initializing S3 for bucket: ${process.env.AWS_BUCKET_NAME} in regio
 const s3 = new S3Client({
     region: process.env.AWS_REGION || 'us-east-1',
     credentials: {
-        accessKeyId: process.env.AWS_ACCESS_KEY_ID,
-        secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+        accessKeyId: process.env.ROTARY_AWS_ACCESS_KEY_ID,
+        secretAccessKey: process.env.ROTARY_AWS_SECRET_ACCESS_KEY,
     },
 });
 
